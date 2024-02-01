7f4307b226994af2f256295110559b1d5333cdb1
naralicecosta <naralicecosta04@gmail.com>
2024-02-01 19:34:04 -0300

    docs: :memo: exercicio de lógica

---
+++ javascript_basico/exercicio/exercicioimagem.js
@@ -0,0 +1,6 @@
+// function ePaisagem(largura, altura){
+//     return largura > altura ? true : false;
+// }
+// console.log(ePaisagem(1920, 1080))
+const ePaisagem = (largura, altura) => largura > altura;
+console.log(ePaisagem(1080, 1920))
