6cb5d89c00a6943557602a71a4f744ff2ff72ab9
naralicecosta <naralicecosta04@gmail.com>
2024-02-15 17:54:59 -0300

    docs: revisando array basico

---
+++ arrays_avancado/basicorarrays.js
@@ -0,0 +1,22 @@
+// const nomes = new Array ('Eduardo', 'Maria', 'Joana')
+// const nomes = ['Eduardo', 'Maria', 'Joana']
+
+// nomes[2] = 'joao'
+// delete nomes[2]
+// console.log(nomes)
+
+
+////////////////////////////////////////////////////////////////
+const nomes = ['Eduardo', 'Maria', 'Joana']
+nomes.push('nara') //adicioanr ao final do array
+nomes.unshift('naralice')
+const removido = nomes.pop() //final do array
+const removido2 = nomes.shift() //começo do array
+console.log(nomes, removido)
+
+/////////////
+const nomes2 = ['Eduardo', 'Maria', 'Joana']
+const nomea = nomes2.join(' ')
+console.log(nomes2)
+
+
