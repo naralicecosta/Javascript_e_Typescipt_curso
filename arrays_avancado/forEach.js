bb4979df78b2c8fe34e4a4e7587458aa0bba9ed2
naralicecosta <naralicecosta04@gmail.com>
2024-02-15 22:58:39 -0300

    docs: foreach

--- arrays_avancado/forEach.js
+++ arrays_avancado/forEach.js
@@ -1,6 +1,17 @@
-const a1 = [10, 20, 30, 4, 5, 6, 7, 8, 9]
-let total = 0
-a1.forEach(valor => {
-    total += valor
+// const a1 = [10, 20, 30, 4, 5, 6, 7, 8, 9]
+// let total = 0
+// a1.forEach(valor => {
+//     total += valor
+// })
+// console.log(total)
+
+const usersList = [
+	{name: 'João', credit: 500},
+	{name: 'Maria', credit: 800}
+];
+
+const newUsersList = usersList.map((user, index, array) => {
+	user.credit += 100;
+	return user;
 })
-console.log(total)
+ console.log(newUsersList);
